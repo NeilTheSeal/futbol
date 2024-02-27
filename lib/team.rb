91ab82cd1c490b7e0376594240b3c59ea176e8e7
Jared Hobson <jaredmhobson@gmail.com>
2024-02-27 15:35:22 -0800

    Add ::create_teams method in team

--- lib/team.rb
+++ lib/team.rb
@@ -1,3 +1,13 @@
 class Team 
-  def initialize; end
+  def initialize(details)
+    @team_id = details[:team_id]
+  end
+
+  def self.create_teams(csv_file_path)
+    teams_list = []
+    CSV.foreach(csv_file_path, headers: true, header_converters: :symbol) do |row|
+        teams_list << Team.new(row)
+    end
+    teams_list
+  end
 end
