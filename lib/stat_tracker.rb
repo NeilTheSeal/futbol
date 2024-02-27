b7d1c4e66e4002747cab777e288e8972f3946342
NeilTheSeal <neil.hendren@gmail.com>
2024-02-27 15:47:03 -0700

    Add from_csv method skeleton

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,3 +1,10 @@
-class StatTracker 
+class StatTracker
   def initialize; end
+
+  def self.from_csv(locations)
+    game_path = locations[:games]
+    teams_path = locations[:teams]
+    game_teams_path = locations[:game_teams]
+    StatTracker.new
+  end
 end
