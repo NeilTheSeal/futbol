04f8972c8a4c83327647b025aafd698348858540
NeilTheSeal <neil.hendren@gmail.com>
2024-02-27 15:42:55 -0700

    Add test for from_csv

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -1,12 +1,21 @@
-require 'spec_helper'
+require "spec_helper"
 
 RSpec.describe StatTracker do
   before(:each) do
-    @stat_tracker = StatTracker.new
+    game_path = "./data/games.csv"
+    team_path = "./data/teams.csv"
+    game_teams_path = "./data/game_teams.csv"
+
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+    @stat_tracker = StatTracker.from_csv(locations)
   end
 
-  describe '#initialize' do
-    it 'exists' do
+  describe "#initialize" do
+    it "exists" do
       expect(@stat_tracker).to be_a(StatTracker)
     end
   end
