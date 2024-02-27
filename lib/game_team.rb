d27ab2e849563e7cf2433cbde3b4aedf7efc9043
Jared Hobson <jaredmhobson@gmail.com>
2024-02-27 15:43:44 -0800

    Add ::create_game_teams method in game_team

--- lib/game_team.rb
+++ lib/game_team.rb
@@ -1,3 +1,13 @@
 class GameTeam
-  def initialize; end
+  def initialize(details)
+    @game_id = details[:game_id]
+  end
+
+  def self.create_game_teams(csv_file_path)
+    game_teams_list = []
+    CSV.foreach(csv_file_path, headers: true, header_converters: :symbol) do |row|
+        game_teams_list << GameTeam.new(row)
+    end
+    game_teams_list
+  end
 end
