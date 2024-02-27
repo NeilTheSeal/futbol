caf7eda168615ba14229b9f697e64a0e4d3b68a4
Jared Hobson <jaredmhobson@gmail.com>
2024-02-27 15:28:10 -0800

    Add ::create_games method in game

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,14 @@
+require 'CSV'
 class Game 
-  def initialize; end
+  def initialize(details)
+    @game_id = details[:game_id]
+  end
+
+  def self.create_games(csv_file_path)
+    games_list = []
+    CSV.foreach(csv_file_path, headers: true, header_converters: :symbol) do |row|
+        games_list << Game.new(row)
+    end
+    games_list
+  end
 end
