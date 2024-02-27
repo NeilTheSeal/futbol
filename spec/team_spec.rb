5fc78dc359e6aae651af759a758d8229dd8bf407
Jared Hobson <jaredmhobson@gmail.com>
2024-02-27 15:34:07 -0800

    Add test for ::create_teams method in team_spec

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -2,12 +2,20 @@ require 'spec_helper'
 
 RSpec.describe Team do
   before(:each) do
-    @team = Team.new
+    @team1 = Team.create_teams('./data/teams.csv')[0]
   end
 
   describe '#initialize' do
     it 'exists' do
-      expect(@team).to be_a(Team)
+      expect(@team1).to be_a(Team)
+    end
+  end
+
+  describe '::create_teams' do
+    it 'creates Team instances from a CSV file' do
+      teams = Team.create_teams('./data/teams.csv')
+
+      expect(teams).to all be_a(Team)
     end
   end
 end
