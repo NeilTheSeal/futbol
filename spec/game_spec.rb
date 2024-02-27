53cecb8bef76323f7f36ecf42c4ab9e0bbce0514
Jared Hobson <jaredmhobson@gmail.com>
2024-02-27 15:27:50 -0800

    Add test for ::create_games method in game_spec

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,12 +2,20 @@ require 'spec_helper'
 
 RSpec.describe Game do
   before(:each) do
-    @game = Game.new
+    @game1 = Game.create_games('./data/games.csv')[0]
   end
 
   describe '#initialize' do
     it 'exists' do
-      expect(@game).to be_a(Game)
+      expect(@game1).to be_a(Game)
+    end
+  end
+
+  describe '::create_games' do
+    it 'creates Game instances from a CSV file' do
+      games = Game.create_games('./data/games.csv')
+
+      expect(games).to all be_a(Game)
     end
   end
 end
