7be7d87b400e719ad2efdd8855c3177a16ce4b89
Jared Hobson <jaredmhobson@gmail.com>
2024-02-27 18:17:20 -0800

    Update instantiations with before all in game_team_spec

--- spec/game_team_spec.rb
+++ spec/game_team_spec.rb
@@ -1,8 +1,12 @@
 require 'spec_helper'
 
 RSpec.describe GameTeam do
+  before(:all) do
+    @game_teams = GameTeam.create_game_teams('./data/game_teams.csv')
+  end
+
   before(:each) do
-    @game_team1 = GameTeam.create_game_teams('./data/game_teams.csv')[0]
+    @game_team1 = @game_teams[0]
   end
 
   describe '#initialize' do
@@ -13,9 +17,7 @@ RSpec.describe GameTeam do
 
   describe '::create_game_teams' do
     it 'creates GameTeam instances from a CSV file' do
-      game_teams = GameTeam.create_game_teams('./data/game_teams.csv')
-
-      expect(game_teams).to all be_a(GameTeam)
+      expect(@game_teams).to all be_a(GameTeam)
     end
   end
 end
