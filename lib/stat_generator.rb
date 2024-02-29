a53549644e18bda920e99e6e85d079532ac91329
Jared Hobson <jaredmhobson@gmail.com>
2024-02-28 21:45:02 -0800

    Add #average_goals_per_game_by_team_when_away and home methods in stat_generator

--- lib/stat_generator.rb
+++ lib/stat_generator.rb
@@ -102,6 +102,18 @@ class StatGenerator
     end
   end
 
+  def total_games_played_by_team_when_away(team_id)
+    @game_teams.count do |game_team|
+      game_team.team_id == team_id && game_team.home_or_away == "away"
+    end
+  end
+
+  def total_games_played_by_team_when_home(team_id)
+    @game_teams.count do |game_team|
+      game_team.team_id == team_id && game_team.home_or_away == "home"
+    end
+  end
+
   def total_goals_by_team(team_id)
     @game_teams.sum do |game_team|
       if game_team.team_id == team_id
@@ -134,6 +146,14 @@ class StatGenerator
 
   def average_goals_per_game_by_team(team_id)
     (total_goals_by_team(team_id).to_f / total_games_played_by_team(team_id)).round(2)
+  end
+
+  def average_goals_per_game_by_team_when_away(team_id)
+    (total_goals_by_team_when_away(team_id).to_f / total_games_played_by_team_when_away(team_id)).round(2)
+  end
+
+  def average_goals_per_game_by_team_when_home(team_id)
+    (total_goals_by_team_when_home(team_id).to_f / total_games_played_by_team_when_home(team_id)).round(2)
   end
 
   def best_offense
